1ac3ab36b5c03828b548923344e085724e658e37
Glisk2 <111385258+Glisk2@users.noreply.github.com>
2024-09-26 14:01:56 -0700

    Player Controller and Camera View
    Updates to Player controller, including sprint feature, as well as adjustments to the camera panning and staying with the player object

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
-    [SerializeField] float MoveSpeed = 10;
-    [SerializeField] float Jump = 5;
+    [SerializeField] float MoveSpeed = 10f;
+    [SerializeField] float SprintMultiplier = 1.5f;
+
+    [SerializeField] Transform cam;
+
+    private bool isSprinting;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +19,40 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal") * MoveSpeed;
-        float verticalInput = Input.GetAxisRaw("Vertical") * MoveSpeed;
+        if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
 
-        rb.velocity = new Vector3(horizontalInput, 0, verticalInput);
-        
-        if(Input.GetButtonDown("Jump")) rb.velocity = new Vector3(rb.velocity.x, Jump, rb.velocity.z);
-    }   
+        //Inputs
+        float horInput = Input.GetAxisRaw("Horizontal") * MoveSpeed;
+        float verInput = Input.GetAxisRaw("Vertical") * MoveSpeed;
+
+        float currentSpeed = isSprinting ? MoveSpeed * SprintMultiplier : MoveSpeed;
+
+        //Camera direction
+        Vector3 camForward = cam.forward;
+        Vector3 camRight = cam.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        //Creating relative cam direction
+        Vector3 forwardRelative = verInput * camForward;
+        Vector3 rightRelative = horInput * camRight;
+
+        Vector3 moveDir = (forwardRelative + rightRelative).normalized * currentSpeed;
+
+        //Movement
+        rb.velocity = new Vector3 (moveDir.x, rb.velocity.y, moveDir.z);
+
+        if(moveDir.magnitude > 0)
+        {
+            transform.forward = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        }
+    }
 }
